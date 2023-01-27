bd5161a509cdfc5f74e9b52754ca70bc2438b7f6
MoniIgnacio <moniignacio02@gmail.com>
2023-01-28 00:49:20 +0400

    add button with handle click done

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,37 @@ export default function App () {
   const [fact, setFact] = useState()
   const [image, setImage] = useState()
 
-  useEffect(() => {
+  const getRandomFact = () => {
     fetch(CAT_ENDPOINT_RANDOM)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-        const firstWord = fact.split(' ', 3).join(' ')
-        // console.log(firtsWord)
-        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImage(url)
-          })
       })
-  }, [])
+  }
+
+  useEffect(getRandomFact, [])
+
+  useEffect(() => {
+      if (!fact) return
+      const firstWord = fact.split(' ', 3).join(' ')
+      // console.log(firtsWord)
+      fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+          const { url } = response
+          setImage(url)
+        })
+  }, [fact])
+
+  const handleClick = () => {
+    getRandomFact()
+  }
 
   return (
     <main>
       <h1>App de gatitos</h1>
+      <button onClick={handleClick}>Refresh</button>
       {fact && <p>{fact}</p>}
       {image && <img src={`${CAT_IMG_URL}${image}`} alt='image-cat' />}
     </main>
